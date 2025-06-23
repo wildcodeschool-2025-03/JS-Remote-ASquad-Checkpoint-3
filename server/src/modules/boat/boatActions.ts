f558cb71470de18a8905efaae19d7090ac9c8d26
NabilY-5 <nabilyahya59280@gmail.com>
2025-06-23 12:20:17 +0200

    feat: step 5 finished

--- server/src/modules/boat/boatActions.ts
+++ server/src/modules/boat/boatActions.ts
@@ -17,13 +17,13 @@ const browse: RequestHandler = async (req, res, next) => {
 
 const edit: RequestHandler = async (req, res, next) => {
   try {
-    const boatToUpdate = {
+    const boat = {
       id: Number(req.params.id),
       coord_x: req.body.coord_x,
       coord_y: req.body.coord_y,
     };
 
-    const affectedRows = await boatRepository.update(boatToUpdate);
+    const affectedRows = await boatRepository.update(boat);
 
     if (affectedRows === 0) {
       res.sendStatus(404);
--- server/src/modules/boat/boatRepository.ts
+++ server/src/modules/boat/boatRepository.ts
@@ -13,11 +13,11 @@ class BoatRepository {
   async readAll(where = {}) {
     // Execute the SQL SELECT query to retrieve all boats from the "boat" table
     const [rows] = await databaseClient.query<Rows>(
-      "select * from boat order by coord_y, coord_x",
+      "SELECT boat.id, boat.name, boat.coord_x, boat.coord_y, tile.id AS tile_id, tile.type, tile.has_treasure FROM boat JOIN tile ON boat.coord_x = tile.coord_x AND boat.coord_y = tile.coord_y ORDER BY boat.coord_y, boat.coord_x",
     );
 
     // Return the array of tiles
-    return rows as Boat[];
+    return rows;
   }
 
   async update(boatToUpdate: Partial<Boat>) {

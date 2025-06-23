80f949d8217ddb99efccf845490d8f2cd48dce20
NICO GARCIA <07nico.garcia@gmail.com>
2025-06-23 10:12:43 +0200

    feat: test step2  tiles route

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -1,7 +1,14 @@
 import type { RequestHandler } from "express";
+import tileRepository from "./tileRepository";
 
 const browse: RequestHandler = async (req, res, next) => {
-  // your code here
+  const tiles = await tileRepository.readAll();
+
+  try {
+    res.json(tiles);
+  } catch (err) {
+    next(err);
+  }
 };
 
 const validate: RequestHandler = async (req, res, next) => {

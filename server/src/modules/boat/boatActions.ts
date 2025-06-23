5569d188a34db7d51d2541ae4d9f9d8962d64f0c
GARY GRAS <gary.gras07@gmail.com>
2025-06-23 10:42:06 +0200

    feat: step3

--- server/src/modules/boat/boatActions.ts
+++ server/src/modules/boat/boatActions.ts
@@ -1,6 +1,7 @@
 import type { RequestHandler } from "express";
-
+import tileRepository from "../tile/tileRepository";
 import boatRepository from "./boatRepository";
+
 
 const browse: RequestHandler = async (req, res, next) => {
   try {
@@ -16,10 +17,22 @@ const browse: RequestHandler = async (req, res, next) => {
 };
 
 const edit: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    const boats = {
+      id: Number(req.params.id),
+      coord_x: req.body.coord_x,
+      coord_y: req.body.coord_y,
+    };
+
+    const affectedRows = await boatRepository.update(boats);
+    if (affectedRows === 0) {
+      res.sendStatus(404);
+    } else {
+      res.sendStatus(204);
+    }
+  } catch (err) {
+    next(err);
+  }
 };
 
-export default {
-  browse,
-  edit,
-};
+export default { browse, edit };

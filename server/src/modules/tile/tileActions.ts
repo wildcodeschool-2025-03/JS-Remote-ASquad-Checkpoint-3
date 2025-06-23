95ee9f7e2eb64870d874d704ee915a9237a1a8b5
Kaneb Florentin <Florentink.pro@gmail.com>
2025-06-23 11:21:28 +0200

    feat: finished test 2

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -1,7 +1,13 @@
 import type { RequestHandler } from "express";
+import client from "../../../database/client";
 
 const browse: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    const [tiles] = await client.query("SELECT * FROM tile");
+    res.json(tiles);
+  } catch (err) {
+    next(err);
+  }
 };
 
 const validate: RequestHandler = async (req, res, next) => {

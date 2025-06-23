11aa6980c90b2dce706f7a0bb27c460420319fa1
MBoussel <mb.boussel@gmail.com>
2025-06-23 12:19:22 +0200

    feat: finish step1, step2, step3, step4, step5

--- server/src/modules/boat/boatRepository.ts
+++ server/src/modules/boat/boatRepository.ts
@@ -13,7 +13,7 @@ class BoatRepository {
   async readAll(where = {}) {
     // Execute the SQL SELECT query to retrieve all boats from the "boat" table
     const [rows] = await databaseClient.query<Rows>(
-      "select * from boat order by coord_y, coord_x",
+      "SELECT boat.id, boat.name, boat.coord_x, boat.coord_y, tile.type, tile.has_treasure FROM boat JOIN tile ON boat.coord_x=tile.coord_x and boat.coord_y=tile.coord_y",
     );
 
     // Return the array of tiles
@@ -22,7 +22,16 @@ class BoatRepository {
 
   async update(boatToUpdate: Partial<Boat>) {
     // your code here
-    return 0;
+    const [result] = await databaseClient.query<Result>(
+      "UPDATE boat SET name = ?, coord_x = ?, coord_y = ? WHERE id = ?",
+      [
+        boatToUpdate.name,
+        boatToUpdate.coord_x,
+        boatToUpdate.coord_y,
+        boatToUpdate.id,
+      ],
+    );
+    return result.affectedRows;
   }
 }
 
--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -1,11 +1,33 @@
 import type { RequestHandler } from "express";
+import tileRepository from "./tileRepository";
 
 const browse: RequestHandler = async (req, res, next) => {
   // your code here
+  try {
+    // Fetch all boats from the database
+    const tiles = await tileRepository.readAll();
+
+    // Respond with the boats in JSON format
+    res.json(tiles);
+  } catch (err) {
+    // Pass any errors to the error-handling middleware
+    next(err);
+  }
 };
 
 const validate: RequestHandler = async (req, res, next) => {
-  // your code here
+  const { coord_x, coord_y } = req.body;
+  const [tile] = await tileRepository.readByCoordinates(coord_x, coord_y);
+
+  if (!tile) {
+    res.sendStatus(422);
+    return;
+  }
+  if (coord_x === undefined || coord_y === undefined) {
+    res.sendStatus(422);
+    return;
+  }
+  next();
 };
 
 export default {
